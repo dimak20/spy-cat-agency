d894ea42639aa1fb7c7e89f6de45dc3eaa7c89bd
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-11-27 14:53:38 +0200

    Add 3rd party apps and drf settings

--- spy_cat_agency/settings.py
+++ spy_cat_agency/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3d party
+    "rest_framework",
+    "drf_spectacular",
+    "django_filters",
     # my apps
     "spycats"
 ]
@@ -122,3 +126,31 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 BREED_NAMES_API = os.getenv("BREED_NAMES_API", None)
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 30,
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Spa Cat Agency API",
+    "DESCRIPTION": "Create Spy Cats and assign them missions",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "defaultModelRendering": "model",
+        "defaultModelsExpandDepth": 2,
+        "defaultModelExpandDepth": 2,
+    },
+}
