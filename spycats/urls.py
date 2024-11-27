39e691b4bbd034bdfc8bbe109f779379a1e2ff3d
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-11-27 14:53:56 +0200

    Register routes for SpyCat and Missions

--- spycats/urls.py
+++ spycats/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from spycats.views import SpyCatViewSet, MissionViewSet
 
 app_name = "spycats"
 
 router = DefaultRouter()
-
+router.register("cats", SpyCatViewSet)
+router.register("missions", MissionViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
