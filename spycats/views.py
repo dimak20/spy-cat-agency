fda71a924665fcf950d29d1a7c1da92fde299de8
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-11-27 14:54:17 +0200

    Add SpyCat View Set and mission view set

--- spycats/views.py
+++ spycats/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from spycats.models import SpyCat, Mission
+from spycats.serializers import CatListSerializer, CatRetrieveSerializer, CatSerializer
+
+
+class SpyCatViewSet(viewsets.ModelViewSet):
+    model = SpyCat
+    queryset = SpyCat.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return CatListSerializer
+        if self.action == "retrieve":
+            return CatRetrieveSerializer
+
+        return CatSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if self.action in ["list", "retrieve"]:
+            return queryset.prefetch_related("missions")
+
+        return queryset
+
+
+class MissionViewSet(viewsets.ModelViewSet):
+    model = Mission
+    queryset = Mission.objects.all()
