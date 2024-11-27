0c1a32c976f167883da2c70630e2cf63f4f54754
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-11-27 13:35:39 +0200

    Add spycat model

--- spycats/models.py
+++ spycats/models.py
@@ -1,3 +1,30 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+from spycats.validators import validate_breed_name
+
+
+class SpyCat(models.Model):
+    name = models.CharField(_("name"), max_length=100)
+    breed = models.CharField(_("breed"), max_length=100)
+    years_of_experience = models.PositiveIntegerField(
+        _("years of experience")
+    )
+    salary = models.DecimalField(
+        _("salary"),
+        max_digits=7,
+        decimal_places=2
+    )
+
+    def __str__(self) -> str:
+        return f"Cat: {self.name}. Breed: {breed}"
+
+    def clean(self):
+        validate_breed_name(self.breed, ValidationError)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+
