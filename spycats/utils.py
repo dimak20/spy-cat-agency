87d5c02d22f048aa54db8f5fd7a09a11a6f12929
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-11-27 13:36:22 +0200

    Add external API request function

---
+++ spycats/utils.py
@@ -0,0 +1,22 @@
+import requests
+from django.conf import settings
+from django.core.cache import cache
+
+BREEDS_API = settings.BREED_NAMES_API
+
+
+def get_breeds_from_api(
+        cache_key: str | None = "static_key",
+        cache_timeout: int | None = 600
+) -> set:
+    cached_data = cache.get(cache_key)
+
+    if cached_data:
+        return cached_data
+
+    response = requests.get(BREEDS_API)
+    response.raise_for_status()
+    data = response.json()
+    cache.set(cache_key, data, timeout=cache_timeout)
+
+    return data
